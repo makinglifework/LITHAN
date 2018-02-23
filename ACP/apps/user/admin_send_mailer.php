ce30f49ec2b9413dcd046ddde551f26861b43dab
Amadeus <amadeus@fourcornerstones.com>
2018-02-23 16:14:50 +0800

    Create email forms

--- ACP/apps/user/admin_send_mailer.php
+++ ACP/apps/user/admin_send_mailer.php
@@ -4,3 +4,28 @@ include '../includes/security.php';
 include '../includes/admin_header.php';
 
 ?>
+	<div class="container">
+		<h1>eMailing</h1>
+		<p>Emailing notification services to members of Developers Network Connection</p>
+		<div class="row">
+			<form method="post" action="">
+				<p>
+					<label for="email">Email:</label>
+					<input type="email" name="email" id="email">
+				</p>
+				<p>
+					<label for="contents">Message</label>
+					<textarea name=contents id="contents"></textarea>
+				</p>
+				<p>
+					<input type="submit" name="send" id="send" value="Send Email">
+				</p>
+			</form>
+			<pre>
+			</pre>
+		</div>
+	</div>
+
+<?php 
+    include '../includes/footer.php';
+?>
